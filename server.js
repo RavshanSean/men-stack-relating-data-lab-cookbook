97dce5a8591620d669fd15b064eeb70fbe7f5f6b
RavshanSean <ravshansean1995@gmail.com>
2024-07-05 13:16:11 -0400

    m

--- server.js
+++ server.js
@@ -6,11 +6,14 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
-
+//new one for comunity page 
+const usersController = require('./controllers/users.js');
+//
 const authController = require('./controllers/auth.js');
 const foodsController = require('./controllers/foods.js');
 const isSignedIn = require('./middleware/is-signed-in.js');
 const passUserToView = require('./middleware/pass-user-to-view.js');
+const User = require('./models/user.js');
 
 
 const port = process.env.PORT ? process.env.PORT : '3000';
@@ -21,7 +24,7 @@ mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 app.use(
@@ -50,6 +53,13 @@ app.use('/auth', authController);
 app.use(isSignedIn);
 // new one
 app.use('/users/:userId/foods',foodsController);
+//new
+// POST /users
+//app.post("/users", async (req, res) => {
+  //await User.create(req.body);
+ // res.redirect("/uers"); // redirect to index fruits
+
+app.use('/users', usersController);   //new
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
